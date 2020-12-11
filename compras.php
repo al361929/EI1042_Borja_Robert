6bed7466e4742ce546e07287e6f7c0ed9602d393
al375874 <al375874@uji.es>
2020-12-11 20:26:16 +0100

    prac 4 terminada

--- compras.php
+++ compras.php
@@ -7,9 +7,9 @@ function comprarMobile ($product){
     $query = "INSERT INTO   compra   (client_id, product_id, date_c)
                         VALUES (?,?,now())";              
     try { 
-        $a=array($_SESSION['client_id'],$product);
+        $a=array(2,$product);
         $consult = $pdo->prepare($query);
-        $a=$consult->execute(array($_SESSION['client_id'],$product));
+        $a=$consult->execute(array(2,$product));
         if (1>$a)return "KO";
         
     } catch (PDOExeption $e) {
@@ -19,8 +19,9 @@ function comprarMobile ($product){
 }
 $productos=$_GET['productos'];
 $res['resultado']="OK";
-if(is_array($productos)){
-    foreach ($productos as $product){
+$prodcs = explode(',', $productos);
+if(is_array($prodcs)){
+    foreach ($prodcs as $product){
         if (comprarMobile((int)$product) === "KO"){
             $res['resultado']="KO";
             break;
